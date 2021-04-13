88f450dcd766fbe282e8a13a2f604c362939044f
liam <liam.lee@email.com>
2021-04-13 20:29:43 +0800

    add batch_crip.sh for multi-threaded operation

---
+++ batch_test/batch_crip.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+start_time=`date +%s`
+
+tmp_fifofile="/tmp/$$.fifo"
+mkfifo $tmp_fifofile
+exec 6<>$tmp_fifofile
+rm $tmp_fifofile
+
+# define max thread numbers
+thread_num=12
+
+for ((i=0;i<${thread_num};i++));do
+	echo
+done >&6
+
+for image in ./*/*.jpg; do
+	read -u6
+	{
+		convert "$image" "${image%.jpg}.png"
+		rm "$image"
+		echo >&6
+	} &
+done
+
+wait
+
+stop_time=`date +%s`
+echo "TIME: `expr $stop_time - $start_time`"
+
+exec 6>&-
+echo "convert finish"
